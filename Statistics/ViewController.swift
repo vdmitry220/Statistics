d28075967d3c7682ecf6e2a5f29b526d565ccac9
DmitryVasechko <dms9961@gmail.com>
2023-08-27 16:12:12 +0300

    first commit

---
+++ Statistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Statistics
+//
+//  Created by Дмитрий Васечко on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
